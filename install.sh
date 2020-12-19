ee870b60d7a24e748bbaec729efade01332d1c17
Mathias <mathias.mellemstuen@gmail.com>
2020-12-19 20:50:11 +0100

    New changes to dotfiles.

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ git pull
 
 cp init.vim ~/.config/nvim/init.vim
 cp .pylintrc ~/.pylintrc
+cp .bashrc ~/.bashrc
 
 echo "Done moving files."
--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
 cp ~/.config/nvim/init.vim init.vim
 cp ~/.pylintrc .pylintrc
+cp ~/.bashrc .bashrc
 
 git add init.vim
 git commit -a -m "New changes to dotfiles."
