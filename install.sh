f8e89ac4546f721f97ea57371c5def72ec9c8670
Mathias <mathias.mellemstuen@gmail.com>
2020-12-30 21:22:55 +0100

    New changes to dotfiles.

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ git pull
 cp init.vim ~/.config/nvim/init.vim
 cp .pylintrc ~/.pylintrc
 cp .bashrc ~/.bashrc
+cp accels.scm ~/.config/xfce4/terminal/accels.scm
 
 echo "Done moving files."
--- upload.sh
+++ upload.sh
@@ -1,10 +1,12 @@
 cp ~/.config/nvim/init.vim init.vim
 cp ~/.pylintrc .pylintrc
 cp ~/.bashrc .bashrc
+cp ~/.config/xfce4/terminal/accels.scm accels.scm
 
 git add init.vim
 git add .pylintrc
 git add .bashrc
+git add accels.scm
 
 git commit -a -m "New changes to dotfiles."
 git push 
